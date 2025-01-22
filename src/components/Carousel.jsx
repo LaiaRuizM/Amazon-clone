7dddc762ed94a38c7aaf83815cf6ce60f0b45e56
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 15:06:57 -0800

    feat: add styles to the Carousel

--- src/components/Carousel.jsx
+++ src/components/Carousel.jsx
@@ -1,7 +1,21 @@
 import { Swiper, SwiperSlide } from "swiper/react"; //Swiper functionalities
+import "swiper/css";
 
 function Carousel() {
-  return <div className="bg-blue-500">Carousel</div>;
+  return (
+    <div className="h-[600px] bg-white">
+      <Swiper loop={true} spaceBetween={0} className="h-[50%]">
+        <SwiperSlide>
+          <img src={"../images/carousel_1.jpg"} />
+        </SwiperSlide>
+        <SwiperSlide>
+          <img src={"../images/carousel_2.jpg"} />
+        </SwiperSlide>
+      </Swiper>
+    </div>
+  );
 }
 
 export default Carousel;
+
+//loop={true} -> Once we get to the end of all the imgs in our Carousel we're just going to go back
--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,6 @@
 //1.- Overall layout of the HomePage (HP)
 //2.- Define the container on the Amzn's HomePage with arbitrary value support (min/max-w-)
-//3.- Create Carousel component and add it to the HP + add Swiper's library to implement the Carousel (npm i swiper) and import some of the Swiper functionalities
+//3.- Create Carousel component and add it to the HP + add Swiper's library to implement the Carousel (npm i swiper) + import some of the Swiper functionalities + add some styling
 
 import { Carousel } from "./";
 
