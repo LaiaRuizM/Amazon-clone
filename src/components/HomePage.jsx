ebd2bf108ebeb22b1a38fed352c0446ba165a7c9
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 14:41:23 -0800

    core: fix the container to sit centrally

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,11 +1,13 @@
 //1.- Overall layout of the homepage
-//2.- Define the container on the Amzn's HomePage with arbitrary value support
+//2.- Define the container on the Amzn's HomePage with arbitrary value support (min/max-w-)
 import React from "react";
 
 const HomePage = () => {
   return (
     <div className="bg-amazonclone-background">
-      <div className="min-w-[1000px] max-w-[1500px] bg-purple-500">Testing</div>
+      <div className="min-w-[1000px] max-w-[1500px] bg-purple-500 m-auto">
+        Testing
+      </div>
     </div>
   );
 };
