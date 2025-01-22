df0af703a3b269e09608910b9891d5de6f06fca2
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 14:38:18 -0800

    feat: Define the container on the Amzn's HomePage with arbitrary value support

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,8 +1,13 @@
 //1.- Overall layout of the homepage
+//2.- Define the container on the Amzn's HomePage with arbitrary value support
 import React from "react";
 
 const HomePage = () => {
-  return <div>HomePage</div>;
+  return (
+    <div className="bg-amazonclone-background">
+      <div className="min-w-[1000px] max-w-[1500px] bg-purple-500">Testing</div>
+    </div>
+  );
 };
 
 export default HomePage;
