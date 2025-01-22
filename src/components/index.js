ad9fec33c35a1953dd4a612b97cb4c15116b7fec
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 14:44:36 -0800

    feat: add Carousel component

--- src/components/index.js
+++ src/components/index.js
@@ -1,2 +1,3 @@
 //This file allow us to import multiple components on one line instead of doing them separately on each line
 export { default as HomePage } from "./HomePage";
+export { default as Carousel } from "./Carousel";
