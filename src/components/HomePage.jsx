a2236473d18a33aef7bef8a7236e5a429255610e
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 18:39:27 -0800

    feat: set up a responsive grid to arrange content in a card-like structure on the homepage

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -1,6 +1,7 @@
 //1.- Overall layout of the HomePage (HP)
 //2.- Define the container on the Amzn's HomePage with arbitrary value support (min/max-w-)
 //3.- Create Carousel component and add it to the HP + add Swiper's library to implement the Carousel (npm i swiper) + import some of the Swiper functionalities + add some styling and config (navigation) + add feature (autoplay)
+//4.- Hop back over to the HP - to start making the content that fit on the HP (arranged in cards and they're in a grid-like fashion)
 
 import { Carousel } from "./";
 
@@ -10,6 +11,12 @@ const HomePage = () => {
       <div className="min-w-[1000px] max-w-[1500px] bg-purple-500 m-auto">
         Testing
         <Carousel />
+        <div className="grid grid-cols-3 xl:grid-cols-4">
+          <div>1</div>
+          <div>2</div>
+          <div>3</div>
+          <div>4</div>
+        </div>
       </div>
     </div>
   );
