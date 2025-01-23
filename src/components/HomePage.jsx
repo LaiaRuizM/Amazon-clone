d9a75ae81470c170914968a27531382d81468961
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-23 11:16:38 -0800

    feat: add banner and style cards to sit over gradient background

--- src/components/HomePage.jsx
+++ src/components/HomePage.jsx
@@ -12,7 +12,7 @@ const HomePage = () => {
       <div className="min-w-[1000px] max-w-[1500px] bg-purple-500 m-auto">
         Testing
         <Carousel />
-        <div className="grid grid-cols-3 xl:grid-cols-4">
+        <div className="grid grid-cols-3 xl:grid-cols-4 -mt-80">
           <HomePageCard
             title={"We have a surprise for you"}
             img={"../images/home_grid_1.jpg"}
@@ -53,6 +53,13 @@ const HomePage = () => {
             img={"../images/home_grid_8.jpg"}
             link={"Learn more"}
           />
+          <div className="m-3 pt-8">
+            <img
+              className="xl:hidden"
+              src={"../images/banner_image_2.jpg"}
+              alt="Banner 2"
+            />
+          </div>
         </div>
       </div>
     </div>
