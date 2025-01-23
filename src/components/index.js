255925b2fff3dd87cf962461f967b9cc2619de39
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-22 18:53:10 -0800

    feat: create reusable HomePageCard component
    This component will be used to structure and display content on the homepage in a consistent, card-like fashion.

--- src/components/index.js
+++ src/components/index.js
@@ -1,3 +1,4 @@
 //This file allow us to import multiple components on one line instead of doing them separately on each line
 export { default as HomePage } from "./HomePage";
 export { default as Carousel } from "./Carousel";
+export { default as HomePageCard } from "./HomePageCard";
