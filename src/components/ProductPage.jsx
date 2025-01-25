efa0a33e330338f1c65be9f355b3408e926e2935
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-24 18:24:08 -0800

    feat: enhance ProductPage with dynamic product fetching
    Added API call to fetch product details dynamically using callAPI and display the product's title on the page. Includes loading state to handle data retrieval delays

--- src/components/ProductPage.jsx
+++ src/components/ProductPage.jsx
@@ -1,8 +1,24 @@
 import { useParams } from "react-router-dom"; //retrieve id
+import { useState, useEffect } from "react";
+import { callAPI } from "../utils/CallApi";
 
 const ProductPage = () => {
   const { id } = useParams();
-  return <div>ProductPage {id} </div>;
+  const [product, setProduct] = useState(null);
+
+  const getProduct = () => {
+    callAPI(`data/products.json`).then(productResults => {
+      setProduct(productResults[id]);
+    });
+  };
+
+  useEffect(() => {
+    getProduct();
+  }, []);
+
+  if (!product?.title) return <h1>Loading Product ...</h1>;
+
+  return product && <div>ProductPage {product.title} </div>;
 };
 
 export default ProductPage;
