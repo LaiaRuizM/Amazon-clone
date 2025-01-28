bba6ba61e537f84cb1674e500dc97b2b6fe72d2f
LaiaRuizM <laia.ruizm@hotmail.com>
2025-01-28 12:15:22 -0800

    feat: integrate Redux for state management

--- src/index.js
+++ src/index.js
@@ -1,6 +1,14 @@
 import ReactDOM from "react-dom";
+import { Provider } from "react-redux";
 import App from "./App";
 import "./index.css";
+import store from "./redux/store";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(<App />); //App is the entry point
+root.render(
+  <Provider store={store}>
+    <App />
+  </Provider>
+);
+
+//The Provider is going to expose our store, so therefore our state to the App component, which is the entry point for our React applitacion. We also need to import our store.
